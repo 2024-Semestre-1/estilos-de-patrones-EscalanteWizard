d128b97b82dc2fd4a2efb556e309def01eef5ca6
EscalanteWizard <jordano.shordi@gmail.com>
2024-05-16 09:31:03 -0600

    docs

---
+++ EjemploAdapter/src/MotorElectricoAdapter.java
@@ -0,0 +1,28 @@
+
+
+public class MotorElectricoAdapter extends Motor{
+    private MotorElectrico motorElectrico;
+
+    public MotorElectricoAdapter(){
+        super();
+        System.out.println("Creando motor electrico adapter...");
+        this.motorElectrico = new MotorElectrico();
+    }
+    @Override
+    public void encender(){
+        System.out.println("Encendiendo el motor electrico adapter...");
+        this.motorElectrico.conectar();
+        this.motorElectrico.activar();
+    }
+    @Override
+    public void acelerar(){
+        System.out.println("Acelerando el motor electrico adapter...");
+        this.motorElectrico.moverMasRapido();
+    }
+    @Override
+    public void apagar(){
+        System.out.println("Apagando el motor electrico adapter...");
+        this.motorElectrico.detener();
+        this.motorElectrico.desconectar();
+    }
+}
